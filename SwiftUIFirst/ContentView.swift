b299ee8587cd0641c0b223bfb703c5ad11e2550c
liaosong <liao_song@outlook.com>
2020-06-11 14:09:05 +0800

    Initial Commit

---
+++ SwiftUIFirst/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIFirst
+//
+//  Created by Song Liao on 6/11/20.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
